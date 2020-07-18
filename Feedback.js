db32ae914f1207d21361a5ad4feb331da70d1ba5
SanityIllicit-ColourBound <68435962+SanityIllicit-ColourBound@users.noreply.github.com>
2020-07-18 14:15:36 +0200

    Add files via upload

---
+++ Feedback.js
@@ -0,0 +1,9 @@
+<script>
+    Userback = window.Userback || {};
+    Userback.access_token = '7239|11134|xtrebtiQ7zSeDG4K0UUYXcLeCHSn6E4Cb4JPVzHN3eskimxxas';
+    (function(id) {
+        var s = document.createElement('script');
+        s.async = 1;s.src = 'https://static.userback.io/widget/v1.js';
+        var parent_node = document.head || document.body;parent_node.appendChild(s);
+    })('userback-sdk');
+</script>
