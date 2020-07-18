353a97ea98ba52501f141d7caeaba0babd30aac0
SanityIllicit-ColourBound <68435962+SanityIllicit-ColourBound@users.noreply.github.com>
2020-07-18 14:16:24 +0200

    Update Feedback.js

--- Feedback.js
+++ Feedback.js
@@ -1,6 +1,6 @@
 <script>
     Userback = window.Userback || {};
-    Userback.access_token = '7239|11134|xtrebtiQ7zSeDG4K0UUYXcLeCHSn6E4Cb4JPVzHN3eskimxxas';
+    Userback.access_token = '9405|17842|HQRUFxWbXpBzpaZ5ILVkqQZFYc4rQVB3yyLfeXwVyDphobnlLy';
     (function(id) {
         var s = document.createElement('script');
         s.async = 1;s.src = 'https://static.userback.io/widget/v1.js';
